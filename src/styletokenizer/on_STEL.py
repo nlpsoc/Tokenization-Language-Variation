ec0c6daee9e71c89613b557e6ca3ee0c79a351f0
AnnaWegmann <anna@prinzip.com>
2024-05-06 17:32:03 -0400

    - anna == roberta-base

--- src/styletokenizer/on_STEL.py
+++ src/styletokenizer/on_STEL.py
@@ -41,10 +41,12 @@ def one_hot_encode(words, vocabulary):
 def eval_tokenizer(tok):
     STEL.eval_on_STEL(style_objects=[TokenizerSimilarity(tok)])
 
+roberta = "roberta-base"
 t5="google-t5/t5-base"
 xlmroberta = "xlm-roberta-base"
 llama27b = "meta-llama/Llama-2-7b-hf"
 mixtreal = "mistralai/Mixtral-8x7B-Instruct-v0.1"
 llama38b = "meta-llama/Meta-Llama-3-8B"
+anna = "AnnaWegmann/Style-Embedding"
 
-eval_tokenizer(tok=tokenizer.get(llama38b))  # "FacebookAI/xlm-roberta-base"
+eval_tokenizer(tok=tokenizer.get(roberta))  # "FacebookAI/xlm-roberta-base"
