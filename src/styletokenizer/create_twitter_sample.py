d6d1abf1318270a10c9b07c20876549f433badcd
AnnaWegmann <anna@prinzip.com>
2024-07-26 08:34:38 -0400

    add twitter sample code

---
+++ src/styletokenizer/create_twitter_sample.py
@@ -0,0 +1,52 @@
+import bz2
+import json
+import os
+from datasets import Dataset
+
+def count_words(text):
+    return len(text.split())
+
+def process_file(file_path, target_word_count_per_file, data):
+    cumulative_word_count = 0
+    with bz2.open(file_path, 'rt') as file:
+        for line in file:
+            tweet = json.loads(line)
+            text = tweet.get("text", "")
+            tweet_id = tweet.get("id", "")
+            word_count = count_words(text)
+            
+            cumulative_word_count += word_count
+            data.append({"id": tweet_id, "text": text})
+            
+            if cumulative_word_count >= target_word_count_per_file:
+                return data, cumulative_word_count
+    return data, cumulative_word_count
+
+
+def sample_texts_from_files(directory, target_word_count):
+    """
+        get the number of compressed files and determine how much to sample from each file,
+        st target word count is distributed equally across bz2 files
+    """
+    files = [os.path.join(root, file_name)
+             for root, _, files in os.walk(directory)
+             for file_name in sorted(files)
+             if not "2022" in file_name and (file_name.endswith('p2.bz2') or file_name.endswith('p1.bz2'))]
+    print(files)
+    num_files = len(files)
+    target_word_count_per_file = target_word_count // num_files
+    data = []
+
+    for filen_index, file_path in enumerate(files):
+        print(f"At file {filen_index}/{num_files} called {file_path}")
+        print(f"Extracting {target_word_count_per_file} words")
+        data, _ = process_file(file_path, target_word_count_per_file, data)
+
+    return data
+
+directory = '/nfs/locker/twitter-decahose-locker/2021'
+output_path = '/shared/3/projects/hiatus/TOKENIZER_wegmann/data/fitting-corpora/twitter'
+target_word_count = 1_500_000_000
+data = sample_texts_from_files(directory, target_word_count)
+dataset = Dataset.from_dict({"id": [item["id"] for item in data], "text": [item["text"] for item in data]})
+dataset.save_to_disk(output_path)
