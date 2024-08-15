4c0e01fc492cf609bb43f1f04144b2cd7fc9d708
AnnaMaria Wegmann <awegmann@n0131.manage.hpc>
2024-08-15 11:38:43 +0200

    add shell for sbatch command

---
+++ src/train_bert.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+### Job name
+#SBATCH --job-name=TRAIN-bert
+
+### File / path where STDOUT & STDERR will be written
+###    %J is the job ID, %I is the array ID
+# ------------------ SENTENCE BERTS -----------------------------------
+#SBATCH -o /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/PRETRAIN_bert-wikibook_%j.txt
+
+### Request the time you need for execution in minutes
+### The format for the parameter is: [hour:]minute:second,
+#SBATCH -t 30:00:00
+
+### Request a specific GPU
+### see https://hpcusers.op.umcutrecht.nl/xwiki/bin/view/Main/Setup/Cluster/GPU-nodes/
+### --> 2g20gb is 1/3 of a A100 GPU
+### --> 7g.79gb is A100 GPU (4 in the whole cluster)
+### SBATCH -p gpu --gpus-per-node=2g.20gb:4
+#SBATCH -p gpu --gpus-per-node=7g.79gb:1
+
+### Memory your job needs per node, e. g. 1 GB
+#SBATCH --mem 120G
+#SBARCH --gres=tmpspace:100G
+
+source /hpc/local/Rocky8/uu_cs_nlpsoc/miniconda3/etc/profile.d/conda.sh
+conda activate /hpc/local/Rocky8/uu_cs_nlpsoc/miniconda3/envs/aw_style_dev
+
+python train_bert.py --uu --test --seed 42 --tokenizer /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/tokenizer/wikipedia-gpt2-32000
