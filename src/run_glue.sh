49a6443d766df0ab194c8fb0f23ae410e3cb3151
AnnaWegmann <anna@prinzip.com>
2024-08-20 17:04:03 +0200

    - GLUE: add WNLI to GLUE script

--- src/run_glue.sh
+++ src/run_glue.sh
@@ -92,3 +92,25 @@ python run_glue.py --model_name_or_path $MODEL_NAME --task_name $TASK_NAME --do_
 python run_glue.py --model_name_or_path $MODEL_NAME --task_name $TASK_NAME --do_train --do_eval \
 --max_seq_length 512 --per_device_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3 \
 --output_dir /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/bert-tiny/$TASK_NAME/44/ --seed 44
+
+export TASK_NAME=cola
+python run_glue.py --model_name_or_path $MODEL_NAME --task_name $TASK_NAME --do_train --do_eval \
+--max_seq_length 512 --per_device_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3 \
+--output_dir /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/bert-tiny/$TASK_NAME/42/ --seed 42
+python run_glue.py --model_name_or_path $MODEL_NAME --task_name $TASK_NAME --do_train --do_eval \
+--max_seq_length 512 --per_device_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3 \
+--output_dir /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/bert-tiny/$TASK_NAME/43/ --seed 43
+python run_glue.py --model_name_or_path $MODEL_NAME --task_name $TASK_NAME --do_train --do_eval \
+--max_seq_length 512 --per_device_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3 \
+--output_dir /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/bert-tiny/$TASK_NAME/44/ --seed 44
+
+export TASK_NAME=wnli
+python run_glue.py --model_name_or_path $MODEL_NAME --task_name $TASK_NAME --do_train --do_eval \
+--max_seq_length 512 --per_device_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 5 \
+--output_dir /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/bert-tiny/$TASK_NAME/42/ --seed 42
+python run_glue.py --model_name_or_path $MODEL_NAME --task_name $TASK_NAME --do_train --do_eval \
+--max_seq_length 512 --per_device_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 5 \
+--output_dir /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/bert-tiny/$TASK_NAME/43/ --seed 43
+python run_glue.py --model_name_or_path $MODEL_NAME --task_name $TASK_NAME --do_train --do_eval \
+--max_seq_length 512 --per_device_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 5 \
+--output_dir /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/bert-tiny/$TASK_NAME/44/ --seed 44
