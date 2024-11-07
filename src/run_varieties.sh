78d8404541f051f670e8de458a4d49476a953522
AnnaWegmann <anna@prinzip.com>
2024-11-07 17:34:10 +0100

    VAR: tiny wiki on DIALECT

--- src/run_varieties.sh
+++ src/run_varieties.sh
@@ -72,9 +72,9 @@ MODEL_NAME="${MODEL_PATH#*/models/}"
 #SEED=42
 #python run_varieties.py --model_path $MODEL_PATH --task $TASK_NAME --seed $SEED --output_dir /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/VAR/$TASK_NAME/$MODEL_NAME/$SEED
 
-#TASK_NAME=DIALECT
-#SEED=42
-#python run_varieties.py --model_path $MODEL_PATH --task $TASK_NAME --seed $SEED --output_dir /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/VAR/$TASK_NAME/$MODEL_NAME/$SEED
+TASK_NAME=DIALECT
+SEED=42
+python run_varieties.py --model_path $MODEL_PATH --task $TASK_NAME --seed $SEED --output_dir /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/VAR/$TASK_NAME/$MODEL_NAME/$SEED
 
 
 ##TASK_NAME=mrpc
