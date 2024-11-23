a6c2ea4b80140afb78c8236018622202dc1d0d8e
AnnaWegmann <anna@prinzip.com>
2024-11-23 21:21:29 +0100

    Renyi: calculated on VARIETIES

--- src/intrinsic_tokenizer_eval.py
+++ src/intrinsic_tokenizer_eval.py
@@ -4,11 +4,14 @@ from styletokenizer.utility.custom_logger import log_and_flush
 from eval_tokenizer import calc_renyi_efficency_from_generator, calc_seq_len_from_generator, calc_avg_tok_from_generator
 from run_glue import task_to_keys as glue_task_to_keys
 from styletokenizer.utility.env_variables import set_cache
+from utility import datasets_helper
 
 set_cache()
 from datasets import load_dataset, load_from_disk
 
 from styletokenizer.utility.tokenizer_vars import OUT_PATH
+from styletokenizer.fitting_corpora import CORPORA_TWITTER, CORPORA_WIKIPEDIA, CORPORA_MIXED
+from styletokenizer.utility.preptraining_corpora import CORPORA_WEBBOOK
 
 BASE_TOKENIZER_PATH = "/hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/tokenizer/"
 BASE_TOKENIZER_PATH = OUT_PATH
@@ -65,8 +68,10 @@ VARIETIES_to_keys = {
 }
 VARIETIES_TASKS = list(VARIETIES_TASK_DICT.keys())
 
+FITTING_CORPORA = [CORPORA_TWITTER, CORPORA_WIKIPEDIA, CORPORA_MIXED, CORPORA_WEBBOOK]
 
-def load_eval_data(task_name_or_hfpath=None, csv_file=None):
+
+def load_eval_data(task_name_or_hfpath=None, csv_file=None, split=None):
     """
         loading the different eval datasets in all different forms (from huggingface datasets,
          locally with datasets, or csv)
@@ -77,7 +82,10 @@ def load_eval_data(task_name_or_hfpath=None, csv_file=None):
     # load dev set of the datasets
     if task_name_or_hfpath is not None:
         if os.path.exists(task_name_or_hfpath):
-            raw_datasets = load_from_disk(task_name_or_hfpath)
+            if split:
+                raw_datasets = load_from_disk(os.path.join(task_name_or_hfpath, split))
+            else:
+                raw_datasets = load_from_disk(task_name_or_hfpath)
             # set task name to last folder in path
             task_name_or_hfpath = os.path.basename(os.path.normpath(task_name_or_hfpath))
         else:
@@ -102,28 +110,37 @@ def load_eval_data(task_name_or_hfpath=None, csv_file=None):
 
 def main():
     # task_name_or_hfpath = "mnli"
-    for task_name_or_hfpath in VARIETIES_TASKS:  # VALUE_PATHS +
+    for task_name_or_hfpath in (FITTING_CORPORA + GLUE_TASKS + VALUE_PATHS + VARIETIES_TASKS):
+        split=None
         if task_name_or_hfpath in VARIETIES_TASK_DICT.keys():
             task = task_name_or_hfpath
             task_name_or_hfpath = VARIETIES_TASK_DICT[task_name_or_hfpath]
             task_to_keys = VARIETIES_to_keys
+        elif task_name_or_hfpath in FITTING_CORPORA:
+            task_to_keys = {"mixed": "text", "twitter": "text", "wikipedia": "text", "webbook": "text"}
+            task = "mixed" # doesn't matter which one
+            split="dev"
         else:
             task = os.path.basename(os.path.normpath(task_name_or_hfpath))
             task_to_keys = glue_task_to_keys
-        raw_datasets = load_eval_data(task_name_or_hfpath)
+        raw_datasets = load_eval_data(task_name_or_hfpath, split=split)
         try:
-            eval_dataset = raw_datasets["validation_matched" if task == "mnli" else "validation"]
-        except AttributeError:  # some of the datasets are not provided in split form
+            if task_name_or_hfpath in FITTING_CORPORA:
+                eval_dataset = raw_datasets["dev"]
+            else:
+                eval_dataset = raw_datasets["validation_matched" if task == "mnli" else "validation"]
+        except KeyError:  # some of the datasets are not provided in split form
             eval_dataset = raw_datasets
         sentence_keys = task_to_keys[task]
         for tokenizer_path in TOKENIZER_PATHS:
             text_generator = (" ".join(example[text_key] for text_key in sentence_keys) for example in eval_dataset)
-            t_gen1, t_gen2, t_gen3 = itertools.tee(text_generator, 3)
+            text_generator, t_gen1, t_gen2, t_gen3 = itertools.tee(text_generator, 4)
             log_and_flush(f"\n{task_name_or_hfpath} - {tokenizer_path}")
             log_and_flush(f"Renyi Efficency: {calc_renyi_efficency_from_generator(t_gen1, tokenizer_path)}")
             log_and_flush(f"Avg Seq Len: {calc_seq_len_from_generator(t_gen2, tokenizer_path)}")
             log_and_flush(f"Avg # Toks/Words + Seq Len (slow impl.): "
                           f"{calc_avg_tok_from_generator(t_gen3, tokenizer_path)}")
+
 
 
 if __name__ == "__main__":
