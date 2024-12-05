37fdba51225e58fe92fe4cd0cca5ce56e3e965c5
AnnaWegmann <anna@prinzip.com>
2024-12-05 16:22:49 +0100

    fit pre-tokenizer

--- src/fit_tokenizer.py
+++ src/fit_tokenizer.py
@@ -43,22 +43,27 @@ def fit_tokenizer(fit_path: str, vocab_size: int, pre_tokenizer: str, dir_name: 
 def init_tokenizer_with_regex(pre_tokenizer):
     # Initialize the BPE tokenizer
     tokenizer = Tokenizer(BPE(unk_token="[UNK]"))
+
+    # select pre-tokenizer
+    # use byte level encoding
+    #   --> with use_regex=False this is only transforming to byte level encoding
+    #       importantly: it is performing no additional split as tested in Pre_Tokenizers.ipynb
+    byte = ByteLevel(add_prefix_space=False, use_regex=False)
     # PRE-TOKENIZATION variable
-    if pre_tokenizer == "ws":
-        # this might be different to what you expect, it is not equivalent to Whitespace(),
-        #   which would be inverted r"\w+|[^\w\s]+"
-        # instead this is already "a step towards" more gpt2/llama 3 like pre-tokenization,
-        #   where the leading whitespace in front of non-whitespace characters is kept,
-        #   otherwise all whitespaces are split off separately
-        # in detail:
-        #       (?!\S)  -->  negative lookahead for non-whitespace character
-        #       \s+     -->  one or more whitespace characters
-        regex_pattern = (
-            r"\s+(?!\S)|\s+"
-        )
-        split = Split(pattern=Regex(regex_pattern), behavior="merged_with_next", invert=False)
-    elif (pre_tokenizer == "gpt2") or (pre_tokenizer == "llama3"):
-        if pre_tokenizer == "gpt2":
+    if (pre_tokenizer == "ws") or (pre_tokenizer == "gpt2") or (pre_tokenizer == "llama3"):
+        if pre_tokenizer == "ws":
+            # this might be different to what you expect, it is not equivalent to Whitespace(),
+            #   which would be inverted r"\w+|[^\w\s]+"
+            # instead this is already "a step towards" more gpt2/llama 3 like pre-tokenization,
+            #   where the leading whitespace in front of non-whitespace characters is kept,
+            #   otherwise all whitespaces are split off separately
+            # in detail:
+            #       (?!\S)  -->  negative lookahead for non-whitespace character
+            #       \s+     -->  one or more whitespace characters
+            regex_pattern = (
+                r"\s+(?!\S)|\s+"
+            )
+        elif pre_tokenizer == "gpt2":
             # regex pattern copied from
             #   https://github.com/facebookresearch/fairseq/blob/bedb259bf34a9fc22073c13a1cee23192fa70ef3/fairseq/data/encoders/gpt2_bpe_utils.py#L70
             regex_pattern = r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
@@ -67,17 +72,14 @@ def init_tokenizer_with_regex(pre_tokenizer):
             #   https://github.com/meta-llama/llama3/blob/main/llama/tokenizer.py#L47
             regex_pattern = (r"(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+["
                              r"\r\n]*|\s*[\r\n]+|\s+(?!\S)|\s+")
-        split = Split(pattern=Regex(regex_pattern),
-                      behavior="merged_with_next", invert=False)
+
+        split = Split(pattern=Regex(regex_pattern), behavior="merged_with_next", invert=False)
+        pre_tokenizer = Sequence([split, byte])
+    elif pre_tokenizer == "no":
+        pre_tokenizer = byte
     else:
         raise ValueError(f"Invalid pre-tokenizer: {pre_tokenizer}")
-    # assert split is a variable
-    assert split is not None
-    # use byte level encoding
-    #   --> with use_regex=False this is only transforming to byte level encoding
-    #       importantly: it is performing no additional split as tested in Pre_Tokenizers.ipynb
-    byte = ByteLevel(add_prefix_space=False, use_regex=False)
-    tokenizer.pre_tokenizer = Sequence([split, byte])
+    tokenizer.pre_tokenizer = pre_tokenizer
     return tokenizer
 
 
@@ -108,7 +110,7 @@ if __name__ == "__main__":
 
     # Add the pre_tokenize argument with choices
     parser.add_argument("--pre_tokenize", type=str, choices=PRE_TOKENIZER, required=True,
-                        help="Specify the pre-tokenization method. Must be one of: 'ws', 'gpt2', 'llama3'.")
+                        help="Specify the pre-tokenization method. Must be one of: 'no', 'ws', 'gpt2', 'llama3'.")
     # parser.add_argument("--test", action="store_true")
 
     args = parser.parse_args()
