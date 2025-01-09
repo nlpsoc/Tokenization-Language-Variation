050cc20ca6c9b3bed36d84d3e1ad46235961aed7
AnnaWegmann <anna@prinzip.com>
2025-01-09 12:56:54 +0100

    train BERT on a different dataset: change loading, add log progress

--- src/styletokenizer/train_data.py
+++ src/styletokenizer/train_data.py
@@ -6,7 +6,7 @@ from styletokenizer.utility.custom_logger import log_and_flush
 
 UMICH_TRAIN_DATASET_PATH = "/shared/3/projects/hiatus/TOKENIZER_wegmann/data/train-corpora/webbook"
 UU_TRAIN_DATASET_PATH = "/hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/data/train-corpora/webbook"
-WEBBOOK_COUNT_PER_ROW = 512
+COUNT_PER_ROW = 512
 
 UU_MIXED_TRAIN_DATASET_PATH = "/hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/data/train-corpora/mixed"
 
@@ -25,9 +25,9 @@ def load_train_dataset(word_count=750_000_000, data_path=UMICH_TRAIN_DATASET_PAT
             train_data.save_to_disk(UU_MIXED_TRAIN_DATASET_PATH)
 
     # for COUNT_PER_ROW get the number of rows to sample for word_count
-    nbr_rows = int(word_count // WEBBOOK_COUNT_PER_ROW)
+    nbr_rows = int(word_count // COUNT_PER_ROW)
     nbr_rows = min(nbr_rows, len(train_data))
-    log_and_flush(f"Using {nbr_rows * WEBBOOK_COUNT_PER_ROW} words for pre-training.")
+    log_and_flush(f"Using {nbr_rows * COUNT_PER_ROW} words for pre-training.")
     # select as many rows as needed to reach the desired train_size, given one row has count COUNT_PER_ROW,
     #   this is not affected by random seed
     if test:
