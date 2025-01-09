dd8b135632f9a3bcc5863ec1979d778964d60b5a
AnnaWegmann <anna@prinzip.com>
2025-01-09 11:01:00 +0100

    train BERT on a different dataset: change loading, add log progress

--- src/styletokenizer/train_data.py
+++ src/styletokenizer/train_data.py
@@ -23,13 +23,13 @@ def load_train_dataset(word_count=750_000_000, data_path=UMICH_TRAIN_DATASET_PAT
             log_and_flush(f"Using {word_count} words for pre-training.")
             train_data = create_dataset_with_fixed_row_length(train_data, word_count)
             train_data.save_to_disk(UU_MIXED_TRAIN_DATASET_PATH)
-    else:
-        # for COUNT_PER_ROW get the number of rows to sample for word_count
-        nbr_rows = int(word_count // WEBBOOK_COUNT_PER_ROW)
-        nbr_rows = min(nbr_rows, len(train_data))
-        log_and_flush(f"Using {nbr_rows * WEBBOOK_COUNT_PER_ROW} words for pre-training.")
-        # select as many rows as needed to reach the desired train_size, given one row has count COUNT_PER_ROW,
-        #   this is not affected by random seed
+
+    # for COUNT_PER_ROW get the number of rows to sample for word_count
+    nbr_rows = int(word_count // WEBBOOK_COUNT_PER_ROW)
+    nbr_rows = min(nbr_rows, len(train_data))
+    log_and_flush(f"Using {nbr_rows * WEBBOOK_COUNT_PER_ROW} words for pre-training.")
+    # select as many rows as needed to reach the desired train_size, given one row has count COUNT_PER_ROW,
+    #   this is not affected by random seed
     if test:
         train_data = train_data.select(range(256))
     else:
