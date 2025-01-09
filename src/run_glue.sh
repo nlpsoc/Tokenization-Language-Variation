03b447c36f77e0dc2f98f22a72950f5f48d02838
AnnaWegmann <anna@prinzip.com>
2025-01-09 13:53:42 +0100

    mVALUE: BERT

--- src/run_glue.sh
+++ src/run_glue.sh
@@ -6,7 +6,7 @@
 ### File / path where STDOUT & STDERR will be written
 ###    %J is the job ID, %I is the array ID
 # ------------------ SENTENCE BERTS -----------------------------------
-#SBATCH -o /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/GLUE-textflint_mixed-train_base-750M-45k_42_%j.txt
+#SBATCH -o /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/GLUE-mVALUE_mixed-train_base-750M-45k_42_%j.txt
 
 ### Request the time you need for execution in minutes
 ### The format for the parameter is: [hour:]minute:second,
