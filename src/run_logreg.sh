85ba5a39d54d77641971e68fa94c0b3a892a7842
AnnaWegmann <anna@prinzip.com>
2025-01-15 16:15:11 +0100

    NUCLE: log reg

--- src/run_logreg.sh
+++ src/run_logreg.sh
@@ -6,7 +6,7 @@
 ### File / path where STDOUT & STDERR will be written
 ###    %J is the job ID, %I is the array ID
 # ------------------ SENTENCE BERTS -----------------------------------
-#SBATCH -o /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/LOGREG_sadiri_test-set_%j.txt
+#SBATCH -o /hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/output/LOGREG_NUCLE_%j.txt
 
 ### Request the time you need for execution in minutes
 ### The format for the parameter is: [hour:]minute:second,
@@ -20,6 +20,6 @@ conda activate /hpc/local/Rocky8/uu_cs_nlpsoc/miniconda3/envs/aw_tokenizer
 
 # python run_logreg.py --task "mnli-textflint,qnli-textflint"  --tokenizer_paths "all"  # qqp-textflint,sst2-textflint,mnli-textflint,qnli-textflint
 # python run_logreg.py --task "mnli-mVALUE,qnli-mVALUE"  --tokenizer_paths "all"
-python run_logreg.py --task "NUCLE"  --tokenizer_paths "all" --on_test_set
-# python run_logreg.py --task "simplification"  --tokenizer_paths "all"
+# python run_logreg.py --task "sadiri"  --tokenizer_paths "all" --on_test_set
+python run_logreg.py --task "NUCLE"  --tokenizer_paths "all"
 # --tokenizer_paths "/hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/tokenizer/pubmed-gpt2-32000/tokenizer.json,/hpc/uu_cs_nlpsoc/02-awegmann/TOKENIZER/tokenizer/webbook-gpt2-32000/tokenizer.json"
