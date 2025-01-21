9dc3fb666ca7b05e4401a2444036f647bc1f922a
AnnaWegmann <anna@prinzip.com>
2025-01-21 18:07:16 +0100

    calculate correlations: collect BERT performances (on textflint only for now)

--- src/calculate_correlations.py
+++ src/calculate_correlations.py
@@ -41,7 +41,7 @@ def main():
             # get tokenizer name
             tokenizer_name = os.path.basename(os.path.dirname(tokenizer_path))
             # get the BERT output for the task
-            result_path = os.path.join(GLUE_OUT_BASE_PATH, tokenizer_name, BERT_PATH, task)
+            result_path = os.path.join(GLUE_OUT_BASE_PATH, tokenizer_name, BERT_PATH, task_key)
             # check that path exists
             if not os.path.exists(result_path):
                 print(f"Path {result_path} does not exist")
